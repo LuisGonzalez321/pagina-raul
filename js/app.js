ff25b131fd6720a2a70fbade2069948aba3e1396
Luis Gabriel <Luis.Gonzalez753@std.uni.edu.ni>
2022-01-10 17:02:42 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+let dataCars = [
+    'Acura',
+    'Alfa Romeo',
+    'Aston Martin',
+    'Audi',
+    'Bentley',
+    'BMW',
+    'Buick',
+    'Cadillac',
+    'Chevrolet',
+    'Chrysler',
+    'Dodge',
+    'Ferrari',
+    'Fiat',
+    'Ford',
+    'GMC',
+    'Honda',
+    'Hummer',
+    'Hyundai',
+    'Infiniti',
+    'ISUZU',
+    'Jaguar',
+    'Jeep',
+    'Kia',
+    'Lamborghini',
+    'Land Rover',
+    'Lexus',
+    'Lincoln',
+    'Lotus,',
+    'Maserati',
+    'Maybach',
+    'Mazda',
+    'McLarenAutomotive',
+    'Mercedez Benz',
+    'Mercury',
+    'Mini',
+    'Mitsubishi',
+    'Nissan',
+
+]
+
+function activeYears (){
+    let years = document.getElementById('years');
+    let year_i = 1980;
+    for (let i = 0; i < 40; i++) {
+        years.innerHTML += `<option value="${year_i + i}">${year_i + i}</option>`;
+        console.log(year_i)
+    }
+}
+activeYears();
+
